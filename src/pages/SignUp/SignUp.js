4345ef79d1e2948cd952e4d3fa5bd922a5bef376
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-28 15:07:38 +0600

    login design

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 const SignUp = () => {
     return (
+        <div className='m-10 '>
+        <h1 className='text-6xl'>Signup</h1>
+
+<div className='m-10 grid grid-cols-1  gap-5 '>
         <div>
-            <h1>sign</h1>
+            <h1>Email:</h1>
+        <input type="email" placeholder="Type email here" className="input input-bordered input-secondary text-rose-900  w-full max-w-xs mt-2" />
         </div>
+        <div>
+            <h1>Photo URL:</h1>
+        <input type="text" placeholder="Photo URL" className="input input-bordered input-secondary text-rose-900  w-full max-w-xs mt-2" />
+        </div>
+
+        <div>
+            <h1>Password:</h1>
+        <input type="password" placeholder="Type password here" className="input input-bordered input-secondary w-full max-w-xs mt-2" />
+        </div>
+        <input type="submit" value="Submit" className="btn bg-rose-900 w-32" />
+</div>
+        
+       
+        
+    </div>
     );
 };
 
--- src/pages/login/LogIn.js
+++ src/pages/login/LogIn.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const LogIn = () => {
     return (
-        <div>
-            <h1>login</h1>
+        <div className='m-10 '>
+            <h1 className='text-6xl'>login</h1>
+
+    <div className='m-10 grid grid-cols-1  gap-5 '>
+            <div>
+                <h1>Email:</h1>
+            <input type="email" placeholder="Type email here" className="input input-bordered input-secondary text-rose-900  w-full max-w-xs mt-2" />
+            </div>
+
+            <div>
+                <h1>Password:</h1>
+            <input type="password" placeholder="Type password here" className="input input-bordered input-secondary w-full max-w-xs mt-2" />
+            </div>
+            <input type="submit" value="Submit" className="btn bg-rose-900 w-32" />
+    </div>
+            
+           
+            
         </div>
     );
 };
