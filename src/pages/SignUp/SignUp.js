0604a0e0eee044c8ce3f5777a041b8375dc362d2
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-28 19:54:44 +0600

    signup

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,39 +1,48 @@
 import React from 'react';
+import { FaGoogle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+  const handleSignup =(event) =>{
+    event.preventDeafult();
+  }
     return (
-        <div className='m-10 text-rose-900 '>
+        <div className='m-10 text-rose-900 text-center'>
  <div className="hero min-h-screen w-auto bg-base-200 ">
   <div className="hero-content flex-col w-full">
     <div className="text-center ">
-      <h1 className="text-5xl font-bold ">Signup now!</h1>
+      <h1 className="text-5xl font-bold ">Sign Up </h1>
       <p className="py-6">For log in ,sign in first.</p>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 rounded-none">
-      <div className="card-body">
+      <form onSubmit={handleSignup} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" name='name' placeholder="Name" className="input input-bordered rounded-none" />
+        </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered rounded-none" />
+          <input type="text" name='email' placeholder="email" className="input input-bordered rounded-none" />
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Phot URL</span>
-          </label>
-          <input type="text" placeholder="photo url" className="input input-bordered rounded-none" />
-        </div>
+
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered rounded-none" />
+          <input type="Password" name='password' placeholder="password" className="input input-bordered rounded-none" />
 
         </div>
         <div className="form-control mt-6">
-          <button className="btn bg-rose-900 rounded-none">Sign up</button>
+          <input className="btn bg-rose-900 rounded-none" type="submit" value="Signup" />
+         
         </div>
-      </div>
+      </form>
+      <h1 className='mx-5 mb-5'>Already signup  <Link to="/login" className=" text-lg font-semibold underline"> Login ..</Link> </h1> 
+      <h1 className='mx-5 mb-5'>or, continue with  <button className="btn bg-rose-900 rounded-none"><FaGoogle></FaGoogle> Google</button></h1> 
     </div>
   </div>
 </div>
--- src/pages/login/LogIn.js
+++ src/pages/login/LogIn.js
@@ -1,36 +1,47 @@
 import React from 'react';
 import {FaGoogle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
+  const handleLogin =(event) =>{
+    event.preventDeafult();
+  }
     return (
-        <div className='m-10 text-rose-900 '>
+        <div className='m-10 text-rose-900 text-center'>
  <div className="hero min-h-screen w-auto bg-base-200 ">
   <div className="hero-content flex-col w-full">
     <div className="text-center ">
       <h1 className="text-5xl font-bold ">Login now!</h1>
       <p className="py-6">For more features ,please log in.</p>
+ 
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 rounded-none">
-      <div className="card-body">
+      <form onSubmit={handleLogin} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered rounded-none" />
+          <input type="text" name='email' placeholder="email" className="input input-bordered rounded-none" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered rounded-none" />
+          <input type="password" name='password' placeholder="password" className="input input-bordered rounded-none" />
 
         </div>
         <div className="form-control mt-6">
-          <button className="btn bg-rose-900 rounded-none">Login</button>
+          <input  className="btn bg-rose-900 rounded-none" type="submit" value="Login" />
+          
         </div>
 
-       <h1>or, continue with  <button className="btn bg-rose-900 rounded-none"><FaGoogle></FaGoogle> Google</button></h1> 
-      </div>
+      
+      </form>
+     
+
+      <h1 className='mx-5 mb-5'>New to SternPhotos  <Link to="/signup" className=" text-lg font-semibold underline"> Signup ..</Link> </h1> 
+
+      <h1 className='mx-5 mb-5'>or, continue with  <button className="btn bg-rose-900 rounded-none"><FaGoogle></FaGoogle> Google</button></h1> 
     </div>
   </div>
 </div>
