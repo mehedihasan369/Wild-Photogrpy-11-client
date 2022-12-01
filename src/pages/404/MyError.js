f41de008f0be714916ebe7c693c3f0d803441446
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-12-01 11:30:21 +0600

    404

--- src/pages/404/MyError.js
+++ src/pages/404/MyError.js
@@ -3,7 +3,8 @@ import React from 'react';
 const MyError = () => {
     return (
         <div>
-            <h1>error;</h1>
+            <h1 className='text-center text-3xl font-bold'>Page Not found...</h1>
+        <img src="https://miro.medium.com/max/691/1*W6sDVrsrLBYHP881a8COQA.jpeg"  alt="" />
         </div>
     );
 };
